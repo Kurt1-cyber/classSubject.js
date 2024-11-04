84af3f9835635683e3e0833834745e3bba32a134
Kurt1-cyber <delutakurtlouie@gmail.com>
2024-11-04 20:07:35 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structure and Algorithms";
+let classSchedule = "Monday, Thursday - 1:30pm to 2:30pm";
+let classroom = "Com Lab A";
+let classInstructor = "Mr. Barayuga";
+let studentName = "Kurt Louie Deluta";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule}. The instructor for the subject is ${classInstructor}.`);
